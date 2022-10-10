b03f764e4b2d2bbe18ec70a39ee2321cebd3aef2
shoaib haque <shoaibhaque7@gmail.com>
2022-10-10 18:32:10 +0600

    added migration file for super admin

---
+++ backend/database/migrations/2022_10_10_122359_create_super_admin_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('super_admin', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username', 5);
+            $table->char('password', 8);
+        });
+
+        \DB::statement('ALTER TABLE super_admin AUTO_INCREMENT = 1;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('super_admin');
+    }
+};

ca6249d5f63439a3e924ab0c5acab3f272f04545
shoaib haque <shoaibhaque7@gmail.com>
2022-10-10 18:39:48 +0600

    added migration file for bank accounts

---
+++ backend/database/migrations/2022_10_10_123358_create_bank_accounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('bank_id', 30)->unique();
+            $table->string('bank_name', 100);
+            $table->char('password', 60);
+        });
+
+        \DB::statement('ALTER TABLE bank_accounts AUTO_INCREMENT = 1;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_accounts');
+    }
+};

28b16e3dee94db8e1c5de9a51684dfeef838030b
shoaib <shoaibhaque7@gmail.com>
2022-10-28 17:10:31 +0600

    altered App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
 
 import Login from "./components/login.component";
 import Register from "./components/register.component";
@@ -13,20 +13,11 @@ import Register from "./components/register.component";
 function App() {
   return (
     <Router>
-      <Navbar bg="primary">
-        <Container>
-          <Link to={"/login"} className="navbar-brand text-white">
-            Login
-          </Link>
-          <Link to={"/register"} className="navbar-brand text-white">
-            Register
-          </Link>
-        </Container>
-      </Navbar>
       <Container className="mt-5">
         <Row>
           <Col md={12}>
             <Routes>
+              <Route path="/" element={<Navigate replace to="/login" />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
             </Routes>

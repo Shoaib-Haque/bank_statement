036d346072c9c570c8d15f806436ba7f80ca81a1
shoaib <shoaibhaque7@gmail.com>
2022-10-31 17:58:09 +0600

    added layout for admin

--- frontend/src/components/Account/Register.component.js
+++ frontend/src/components/Account/Register.component.js
@@ -6,6 +6,7 @@ import Col from "react-bootstrap/Col";
 import axios from "axios";
 import Swal from "sweetalert2";
 import { useNavigate, Link } from "react-router-dom";
+import Layout from "../Layout/Admin/Layout.Component";
 
 export default function Register() {
   const navigate = useNavigate();
@@ -45,91 +46,93 @@ export default function Register() {
   };
 
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-12 col-sm-12 col-md-6">
-          <div className="card">
-            <div className="card-body">
-              <h4 className="card-bank_id">Register</h4>
-              <hr />
-              <div className="form-wrapper">
-                {Object.keys(validationError).length > 0 && (
-                  <div className="row">
-                    <div className="col-12">
-                      <div className="alert alert-danger">
-                        <ul className="mb-0">
-                          {Object.entries(validationError).map(
-                            ([key, value]) => (
-                              <li key={key}>{value}</li>
-                            )
-                          )}
-                        </ul>
+    <Layout>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-sm-12 col-md-6">
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-bank_id">Register</h4>
+                <hr />
+                <div className="form-wrapper">
+                  {Object.keys(validationError).length > 0 && (
+                    <div className="row">
+                      <div className="col-12">
+                        <div className="alert alert-danger">
+                          <ul className="mb-0">
+                            {Object.entries(validationError).map(
+                              ([key, value]) => (
+                                <li key={key}>{value}</li>
+                              )
+                            )}
+                          </ul>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                )}
-                <Form onSubmit={register}>
-                  <Row>
-                    <Col>
-                      <Form.Group controlId="bank_id">
-                        <Form.Label>bank Id</Form.Label>
-                        <Form.Control
-                          type="text"
-                          value={bank_id}
-                          onChange={(event) => {
-                            setBankId(event.target.value);
-                          }}
-                        />
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col>
-                      <Form.Group controlId="bank_name">
-                        <Form.Label>Bank Name</Form.Label>
-                        <Form.Control
-                          type="text"
-                          value={bank_name}
-                          onChange={(event) => {
-                            setBankName(event.target.value);
-                          }}
-                        />
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col>
-                      <Form.Group controlId="Password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                          type="password"
-                          value={password}
-                          onChange={(event) => {
-                            setPassword(event.target.value);
-                          }}
-                        />
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col sm={10}>
-                      <Button
-                        variant="primary"
-                        className="mt-2"
-                        size="lg"
-                        block="block"
-                        type="submit"
-                      >
-                        Register
-                      </Button>
-                    </Col>
-                  </Row>
-                </Form>
+                  )}
+                  <Form onSubmit={register}>
+                    <Row>
+                      <Col>
+                        <Form.Group controlId="bank_id">
+                          <Form.Label>bank Id</Form.Label>
+                          <Form.Control
+                            type="text"
+                            value={bank_id}
+                            onChange={(event) => {
+                              setBankId(event.target.value);
+                            }}
+                          />
+                        </Form.Group>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <Form.Group controlId="bank_name">
+                          <Form.Label>Bank Name</Form.Label>
+                          <Form.Control
+                            type="text"
+                            value={bank_name}
+                            onChange={(event) => {
+                              setBankName(event.target.value);
+                            }}
+                          />
+                        </Form.Group>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <Form.Group controlId="Password">
+                          <Form.Label>Password</Form.Label>
+                          <Form.Control
+                            type="password"
+                            value={password}
+                            onChange={(event) => {
+                              setPassword(event.target.value);
+                            }}
+                          />
+                        </Form.Group>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col sm={10}>
+                        <Button
+                          variant="primary"
+                          className="mt-2"
+                          size="lg"
+                          block="block"
+                          type="submit"
+                        >
+                          Register
+                        </Button>
+                      </Col>
+                    </Row>
+                  </Form>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 }
---
+++ frontend/src/components/Layout/Admin/Navigation.component.js
@@ -0,0 +1,68 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+
+const Navigation = () => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("authToken");
+
+  const logout = async () => {
+    const formData = new FormData();
+    formData.append("token", token);
+    await axios
+    .post(`http://127.0.0.1:8000/api/admin/logout`, formData, {headers: { Authorization: `Bearer ${token}`}})
+    .then(response => {
+      if (response.status === 200) {
+        localStorage.removeItem("authToken");
+        localStorage.removeItem("role");
+        navigate("/login");
+      } else {
+        console.log(response);
+      }
+    })
+    .catch(({ response }) => {
+      console.log(response);
+    });
+  };
+
+  return (
+    <Navbar bg="light" expand="lg" sticky="top">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Button
+              variant=""
+              className=""
+              size="sm"
+              block="block"
+              type="button"
+              onClick={logout}
+            >
+              Logout
+            </Button>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navigation;

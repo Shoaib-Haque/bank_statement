c8bf72ba9b21d336642fb4e94d1c6a2e9fc1a0a6
shoaib <shoaibhaque7@gmail.com>
2022-11-07 10:14:30 +0600

    altered navigation for admin

--- frontend/src/components/Layout/Admin/Navigation.component.js
+++ frontend/src/components/Layout/Admin/Navigation.component.js
@@ -3,6 +3,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
 
@@ -32,14 +33,18 @@ const Navigation = () => {
   };
 
   return (
-    <Navbar bg="light" expand="lg" sticky="top">
+    <Navbar bg="light" expand="lg" sticky="top" className="mb-2">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/accounts">Home</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Button
+            <NavDropdown title="Action" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/accounts/create">Create Account</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline >
+              <Button
               variant=""
               className=""
               size="sm"
@@ -49,18 +54,7 @@ const Navigation = () => {
             >
               Logout
             </Button>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
+            </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>

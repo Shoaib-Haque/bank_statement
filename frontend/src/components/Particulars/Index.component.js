11be7a3573e6c0c5228b9882d36c29108d6b767f
shoaib haque <shoaibhaque7@gmail.com>
2022-11-12 23:44:25 +0600

    added component for showing list of particulars

--- frontend/src/components/Particulars/Index.component.js
+++ frontend/src/components/Particulars/Index.component.js
@@ -1,47 +1,239 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { useNavigate, Link } from "react-router-dom";
+import Layout from "../Layout/Admin/Layout.Component";
+import Css from "./Index.css";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Dropdown from "react-bootstrap/Dropdown";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+import BootstrapTable from "react-bootstrap-table-next";
+import ToolkitProvider, {Search, CSVExport} from "react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit";
+import Loader from "../Loader/Loader.component";
 
 export default function Particulars() {
-    let [particulars, setParticulars] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const token = localStorage.getItem("authToken");
+  const navigate = useNavigate();
+  let [list, setList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const token = localStorage.getItem("authToken");
+  const { SearchBar, ClearSearchButton } = Search;
+  const { ExportCSVButton } = CSVExport;
+  const columns = [
+    {
+      dataField: "sl.no",
+      text: "#",
+      csvExport: false,
+      formatter: (cell, row, rowIndex, formatExtraData) => {
+        return rowIndex + 1;
+      },
+    },
+    {
+      dataField: "particulars",
+      text: "Particulars",
+      sort: true,
+    },
+    {
+      dataField: "link",
+      text: "ACTION",
+      csvExport: false,
+      formatter: (rowContent, row) => {
+        return (
+          <Dropdown className="buttons_dropdown">
+            <Dropdown.Toggle
+              variant="default"
+              id="dropdown-basic"
+              className="pt-0"
+            >
+              Action
+            </Dropdown.Toggle>
+            <Dropdown.Menu className="dropdown-menu-action">
+              <Dropdown.Item>
+                <Link to={`/particulars/${row.id}/edit`}>
+                  <Button
+                    type="button"
+                    variant="success"
+                    size="sm"
+                    className="btn btn-block button"
+                  >
+                    Edit
+                  </Button>
+                </Link>
+              </Dropdown.Item>
+              <Dropdown.Item>
+                <Button
+                  type="button"
+                  variant="danger"
+                  size="sm"
+                  className="btn btn-block button"
+                  onClick={() => destroy(row.id)}
+                >
+                  Delete
+                </Button>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        );
+      },
+    },
+  ];
 
     useEffect(() => {
-        getParticulars();
+        index();
     }, []);
 
-    const getParticulars = () => {
-        setLoading(true);
-        fetch(`${process.env.REACT_APP_API_BASE_URL}admin/particulars`, {
+    const index = async () => {
+      setLoading(true);
+      await axios
+        .get(`${process.env.REACT_APP_API_BASE_URL}particulars`, {
           headers: { Authorization: `Bearer ${token}` },
         })
-          .then((response) => response.json())
-          .then((data) => {
-            setParticulars(data.particulars);
-            setLoading(false);
-          })
-          .catch((error) => {
-            console.log(error);
-            setLoading(false);
+        .then(({ data }) => {
+          setList(data);
+        })
+        .catch(({ response }) => {
+          console.log(response.data);
+          if (response.status === 401) {
+            localStorage.clear();
+            navigate("/login");
+          } else {
+            Swal.fire({
+              text: response.data.message,
+              icon: "error",
+            });
+          }
+        });
+      setLoading(false);
+    };
+
+    const destroy = async (id) => {
+      const isConfirm = await Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then((result) => {
+        return result.isConfirmed;
+      });
+  
+      if (!isConfirm) {
+        return;
+      }
+  
+      await axios
+        .delete(`${process.env.REACT_APP_API_BASE_URL}particulars/${id}`, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then(({ data }) => {
+          Swal.fire({
+            text: data.message,
+            icon: "success",
           });
+          setList((list) => list.filter((item, i) => item.id !== id));
+        })
+        .catch(({ response }) => {
+          console.log(response.data);
+          if (response.status === 401) {
+            localStorage.clear();
+            navigate("/login");
+          } else {
+            Swal.fire({
+              text: response.data.message,
+              icon: "error",
+            });
+          }
+        });
     };
 
     return (
         <>
       {loading ? (
-        <div>...Data Loading.....</div>
+        <Loader />
       ) : (
-        <div>
-          <tbody>
-          <tr>
-            <th>Particulars</th>
-          </tr>
-            {particulars.map((item, i) => (
-                <tr key={i}>
-                <td>{item.particulars}</td>
-                </tr>
-            ))}
-        </tbody>
-        </div>
+        <Layout>
+          <Container>
+            <Row className="justify-content-md-center">
+              <Col md={8}>
+                <Card>
+                  <Card.Header>Particulars List</Card.Header>
+                  <Card.Body>
+                    {list.length ? (
+                      <ToolkitProvider
+                        keyField="Id"
+                        data={list}
+                        columns={columns}
+                        search
+                        bootstrap4
+                      >
+                        {(props) => (
+                          <Row>
+                            <Col>
+                              <Row>
+                                <Col>
+                                  <Card.Title>List of Particulars</Card.Title>
+                                </Col>
+                                <Col>
+                                  <ExportCSVButton
+                                    {...props.csvProps}
+                                    className="btn-outline-secondary csv-button d-block d-md-none"
+                                  >
+                                    Export CSV
+                                  </ExportCSVButton>
+                                </Col>
+                              </Row>
+                              <Row>
+                                <Col>
+                                  <SearchBar
+                                    {...props.searchProps}
+                                    srText=""
+                                    placeholder="Search..."
+                                    className="search-bar"
+                                  />
+                                  <ClearSearchButton
+                                    {...props.searchProps}
+                                    className="btn-outline-secondary"
+                                  />
+                                  <ExportCSVButton
+                                    {...props.csvProps}
+                                    className="btn-outline-secondary csv-button d-none d-md-block"
+                                  >
+                                    Export CSV
+                                  </ExportCSVButton>
+                                </Col>
+                              </Row>
+                              <Row className="mt-2 scroll-div">
+                                <Col>
+                                  <BootstrapTable
+                                    bootstrap4
+                                    width="100vw;"
+                                    striped
+                                    hover
+                                    condensed
+                                    {...props.baseProps}
+                                  ></BootstrapTable>
+                                </Col>
+                              </Row>
+                            </Col>
+                          </Row>
+                        )}
+                      </ToolkitProvider>
+                    ) : (
+                      <Row>
+                        <Col>Nothing to Show</Col>
+                      </Row>
+                    )}
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </Layout>
       )}
     </>
     )

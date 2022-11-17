ec753e35996f462b08def552978b7dd5fa27e270
shoaib <shoaibhaque7@gmail.com>
2022-11-17 18:47:37 +0600

    added statement list page

---
+++ backend/app/Http/Controllers/Account/Statement/StatementController.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Http\Controllers\Account\Statement;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Statements;
+use Exception;
+use Illuminate\Validation\Rule;
+
+class StatementController extends Controller
+{
+    /**
+     * Create a new StatementController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:account');
+        config(['auth.defaults.guard' => 'account']);
+    }
+
+    /**
+     * Get Statement List
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        return Statements::select('*')->get();
+    }
+
+    /**
+     * Create Statement.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function create(Request $request)
+    {
+        $request->validate([
+            'particulars_id' => 'required',
+            'amount' => 'required',
+            'entry' => 'required',
+            'date' => 'required',
+        ]);
+
+        try {
+            Statements::create([
+                'bank_id' => auth()->user()->bank_id, //auth()->user()->id;
+                'particulars_id' => $request->particulars_id,
+                'amount' => floatval($request->amount),
+                'entry' => $request->entry,
+                'date' => $request->date
+            ]);
+
+            return response()->json([
+                'message' => 'Statement Saved!!'
+            ]);
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            return response()->json([
+                'message' => 'Something goes wrong while storing Statement Information!!'
+            ], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        try {
+            $statement = Statements::find($id);
+            if ($statement) {
+                return response()->json([
+                    'statement' => $statement
+                ]);
+            }
+            return response()->json([
+                'message' => 'Record not found!!'
+            ], 404);
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            return response()->json([
+                'message' => 'Something went wrong!!'
+            ], 500);
+        }
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'particulars_id' => 'required',
+            'amount' => 'required',
+            'entry' => 'required',
+            'date' => 'required',
+        ]);
+
+        try {
+            $statement = Statements::find($id);
+            $statement->fill([
+                'bank_id' => auth()->user()->bank_id,
+                'particulars_id' => $request->particulars_id,
+                'amount' => floatval($request->amount),
+                'entry' => $request->entry,
+                'date' => $request->date
+            ])->update();
+            return response()->json([
+                'message' => 'Statement Updated Successfully!!'
+            ]);
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            return response()->json([
+                'message' => 'Something goes wrong while updating Statement!!'
+            ], 500);
+        }
+    }
+
+    /**
+     * Destroy the specified resource in storage.
+     *
+     * @param  int  $id
+     */
+    public function destroy($id)
+    {
+        try {
+            $statement = Statements::find($id);
+            $statement->delete();
+            return response()->json([
+                'message' => 'Statement Deleted Successfully!!'
+            ]);
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            return response()->json([
+                'message' => 'Something goes wrong while deleting Statement!!'
+            ], 500);
+        }
+    }
+}
--- frontend/src/components/Statement/Index.component.js
+++ frontend/src/components/Statement/Index.component.js
@@ -1,44 +1,265 @@
 import React, { useEffect, useState } from "react";
-import Layout from "../Layout/Account/Layout.Component";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 
-export default function Login() {
-  const [bank_id, setBankId] = useState("");
-  const [bank_name, setBankName] = useState("");
-  const [loading, setLoading] = useState(false);
+import Layout from "../Layout/Admin/Layout.Component";
+import Loader from "../Loader/Loader.component";
+
+import Swal from "sweetalert2";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+import BootstrapTable from "react-bootstrap-table-next";
+import ToolkitProvider, {Search, CSVExport} from "react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit";
+
+import Card from "react-bootstrap/Card";
+import Dropdown from "react-bootstrap/Dropdown";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export default function Index() {
+  const navigate = useNavigate();
+  const [TITLE, setTitle] = useState("");
   const token = localStorage.getItem("authToken");
+  const [loading, setLoading] = useState(false);
+
+  const [list, setList] = useState([]);
+  const columns = [
+    {
+      dataField: "sl.no",
+      text: "#",
+      csvExport: false,
+      formatter: (cell, row, rowIndex, formatExtraData) => {
+        return rowIndex + 1;
+      },
+    },
+    {
+      dataField: "bank_id",
+      text: "Bank ID",
+      sort: true,
+    },
+    {
+      dataField: "amount",
+      text: "Amount",
+      sort: true,
+    },
+    {
+      dataField: "link",
+      text: "ACTION",
+      csvExport: false,
+      formatter: (rowContent, row) => {
+        return (
+          <Dropdown className="buttons-dropdown">
+            <Dropdown.Toggle
+              variant="default"
+              id="dropdown-basic"
+              className="pt-0"
+            >
+              Action
+            </Dropdown.Toggle>
+            <Dropdown.Menu className="dropdown-menu-action">
+              <Dropdown.Item>
+                <Link to={`/statements/${row.id}/edit`}>
+                  <Button
+                    type="button"
+                    variant="success"
+                    size="sm"
+                    className="btn btn-block button"
+                  >
+                    Edit
+                  </Button>
+                </Link>
+              </Dropdown.Item>
+              <Dropdown.Item>
+                <Button
+                  type="button"
+                  variant="danger"
+                  size="sm"
+                  className="btn btn-block button"
+                  onClick={() => destroy(row.id)}
+                >
+                  Delete
+                </Button>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        );
+      },
+    },
+  ];
+  const { SearchBar, ClearSearchButton } = Search;
+  const { ExportCSVButton } = CSVExport;
 
   useEffect(() => {
-    getStatement();
+    index();
   }, []);
 
-  const getStatement = () => {
+  const index = async () => {
     setLoading(true);
-    fetch(`${process.env.REACT_APP_API_BASE_URL}account/account-profile`, {
-      headers: { Authorization: `Bearer ${token}` },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setBankId(data.bank_id);
-        setBankName(data.bank_name);
-        setLoading(false);
+    await axios
+      .get(`${process.env.REACT_APP_API_BASE_URL}statements`, {
+        headers: { Authorization: `Bearer ${token}` },
       })
-      .catch((error) => {
-        console.log(error);
-        setLoading(false);
+      .then(({ data }) => {
+        setList(data);
+        console.log(data);
+        setTitle("Statement List");
+      })
+      .catch(({ response }) => {
+        console.log(response.data);
+        if (response.status === 401) {
+          localStorage.clear();
+          navigate("/login");
+        } else {
+          Swal.fire({
+            text: response.data.message,
+            icon: "error",
+          });
+        }
+      });
+    setLoading(false);
+  };
+
+  const destroy = async (id) => {
+    const isConfirm = await Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      return result.isConfirmed;
+    });
+
+    if (!isConfirm) {
+      return;
+    }
+
+    await axios
+      .delete(`${process.env.REACT_APP_API_BASE_URL}Statements/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then(({ data }) => {
+        Swal.fire({
+          text: data.message,
+          icon: "success",
+        });
+        setList((list) => list.filter((item, i) => item.id !== id));
+      })
+      .catch(({ response }) => {
+        console.log(response.data);
+        if (response.status === 401) {
+          localStorage.clear();
+          navigate("/login");
+        } else {
+          Swal.fire({
+            text: response.data.message,
+            icon: "error",
+          });
+        }
       });
   };
 
   return (
-    <Layout>
+    <>
       {loading ? (
-        <div>...Data Loading.....</div>
+        <Loader />
       ) : (
-        <div>
-          <span>{bank_id}</span>
-          <br />
-          <span>{bank_name}</span>
-        </div>
+        <Layout TITLE={TITLE}>
+          <Row className="justify-content-center text-nowrap">
+            <Col xs={11} sm={10} md={8} lg={6} xl={5}>
+              <Card>
+                <Card.Header>
+                  <Row>
+                    <Col>Statement List</Col>
+                    <Col className="d-flex justify-content-end">
+                      <Link to={`/Statements/create`}>
+                        <Button
+                          type="button"
+                          variant="primary"
+                          size="sm"
+                          className="btn btn-block button"
+                        >
+                          Create
+                        </Button>
+                      </Link>
+                    </Col>
+                  </Row>
+                </Card.Header>
+                <Card.Body>
+                  {list.length ? (
+                    <ToolkitProvider
+                      keyField="Id"
+                      data={list}
+                      columns={columns}
+                      search
+                      bootstrap4
+                    >
+                      {(props) => (
+                        <Row>
+                          <Col>
+                            <Row>
+                              <Col>
+                                <Card.Title>List of Statements</Card.Title>
+                              </Col>
+                              <Col>
+                                <ExportCSVButton
+                                  {...props.csvProps}
+                                  className="btn-outline-secondary d-block d-sm-none csv-button"
+                                >
+                                  Export CSV
+                                </ExportCSVButton>
+                              </Col>
+                            </Row>
+                            <Row className="mt-1">
+                              <Col>
+                                <SearchBar
+                                  {...props.searchProps}
+                                  srText=""
+                                  placeholder="Search..."
+                                  className="search-bar"
+                                />
+                                <ClearSearchButton
+                                  {...props.searchProps}
+                                  className="btn-outline-secondary"
+                                />
+                                <ExportCSVButton
+                                  {...props.csvProps}
+                                  className="btn-outline-secondary d-none d-sm-block csv-button"
+                                >
+                                  Export CSV
+                                </ExportCSVButton>
+                              </Col>
+                            </Row>
+                            <Row className="mt-2 scroll-div">
+                              <Col>
+                                <BootstrapTable
+                                  bootstrap4
+                                  width="100vw;"
+                                  striped
+                                  hover
+                                  condensed
+                                  wrapperClasses="table-responsive"
+                                  {...props.baseProps}
+                                ></BootstrapTable>
+                              </Col>
+                            </Row>
+                          </Col>
+                        </Row>
+                      )}
+                    </ToolkitProvider>
+                  ) : (
+                    <Row>
+                      <Col>Nothing to Show</Col>
+                    </Row>
+                  )}
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Layout>
       )}
-    </Layout>
+    </>
   );
 }

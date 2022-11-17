a424013dff5facfdaa8ea18955bd96a159311c3b
shoaib <shoaibhaque7@gmail.com>
2022-11-17 12:53:13 +0600

    fixed particulars list page

--- frontend/src/components/Particulars/Index.component.js
+++ frontend/src/components/Particulars/Index.component.js
@@ -1,27 +1,28 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Swal from "sweetalert2";
 import { useNavigate, Link } from "react-router-dom";
+
 import Layout from "../Layout/Admin/Layout.Component";
-import Css from "./Index.css";
-import Container from "react-bootstrap/Container";
+import Loader from "../Loader/Loader.component";
+
+import Swal from "sweetalert2";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+import BootstrapTable from "react-bootstrap-table-next";
+import ToolkitProvider, {Search, CSVExport} from "react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit";
+
 import Card from "react-bootstrap/Card";
 import Dropdown from "react-bootstrap/Dropdown";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
-import BootstrapTable from "react-bootstrap-table-next";
-import ToolkitProvider, {Search, CSVExport} from "react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit";
-import Loader from "../Loader/Loader.component";
 
-export default function Particulars() {
+export default function Index() {
   const navigate = useNavigate();
-  let [list, setList] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [TITLE, setTitle] = useState("");
   const token = localStorage.getItem("authToken");
-  const { SearchBar, ClearSearchButton } = Search;
-  const { ExportCSVButton } = CSVExport;
+  const [loading, setLoading] = useState(false);
+
+  const [list, setList] = useState([]);
   const columns = [
     {
       dataField: "sl.no",
@@ -42,7 +43,7 @@ export default function Particulars() {
       csvExport: false,
       formatter: (rowContent, row) => {
         return (
-          <Dropdown className="buttons_dropdown">
+          <Dropdown className="buttons-dropdown">
             <Dropdown.Toggle
               variant="default"
               id="dropdown-basic"
@@ -80,85 +81,88 @@ export default function Particulars() {
       },
     },
   ];
+  const { SearchBar, ClearSearchButton } = Search;
+  const { ExportCSVButton } = CSVExport;
 
-    useEffect(() => {
-        index();
-    }, []);
+  useEffect(() => {
+    index();
+  }, []);
 
-    const index = async () => {
-      setLoading(true);
-      await axios
-        .get(`${process.env.REACT_APP_API_BASE_URL}particulars`, {
-          headers: { Authorization: `Bearer ${token}` },
-        })
-        .then(({ data }) => {
-          setList(data);
-        })
-        .catch(({ response }) => {
-          console.log(response.data);
-          if (response.status === 401) {
-            localStorage.clear();
-            navigate("/login");
-          } else {
-            Swal.fire({
-              text: response.data.message,
-              icon: "error",
-            });
-          }
-        });
-      setLoading(false);
-    };
-
-    const destroy = async (id) => {
-      const isConfirm = await Swal.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!",
-      }).then((result) => {
-        return result.isConfirmed;
-      });
-
-      if (!isConfirm) {
-        return;
-      }
-
-      await axios
-        .delete(`${process.env.REACT_APP_API_BASE_URL}particulars/${id}`, {
-          headers: { Authorization: `Bearer ${token}` },
-        })
-        .then(({ data }) => {
+  const index = async () => {
+    setLoading(true);
+    await axios
+      .get(`${process.env.REACT_APP_API_BASE_URL}particulars`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then(({ data }) => {
+        setList(data);
+        setTitle("Particulars List");
+      })
+      .catch(({ response }) => {
+        console.log(response.data);
+        if (response.status === 401) {
+          localStorage.clear();
+          navigate("/login");
+        } else {
           Swal.fire({
-            text: data.message,
-            icon: "success",
+            text: response.data.message,
+            icon: "error",
           });
-          setList((list) => list.filter((item, i) => item.id !== id));
-        })
-        .catch(({ response }) => {
-          console.log(response.data);
-          if (response.status === 401) {
-            localStorage.clear();
-            navigate("/login");
-          } else {
-            Swal.fire({
-              text: response.data.message,
-              icon: "error",
-            });
-          }
-        });
-    };
+        }
+      });
+    setLoading(false);
+  };
 
-    return (
-        <>
+  const destroy = async (id) => {
+    const isConfirm = await Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      return result.isConfirmed;
+    });
+
+    if (!isConfirm) {
+      return;
+    }
+
+    await axios
+      .delete(`${process.env.REACT_APP_API_BASE_URL}particulars/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then(({ data }) => {
+        Swal.fire({
+          text: data.message,
+          icon: "success",
+        });
+        setList((list) => list.filter((item, i) => item.id !== id));
+      })
+      .catch(({ response }) => {
+        console.log(response.data);
+        if (response.status === 401) {
+          localStorage.clear();
+          navigate("/login");
+        } else {
+          Swal.fire({
+            text: response.data.message,
+            icon: "error",
+          });
+        }
+      });
+  };
+
+  return (
+    <>
       {loading ? (
         <Loader />
       ) : (
-        <Layout>
-          <Row className="justify-content-md-center">
-            <Col md={8}>
+        <Layout TITLE={TITLE}>
+          <Row className="justify-content-center text-nowrap">
+            <Col xs={11} sm={10} md={8} lg={6} xl={5}>
               <Card>
                 <Card.Header>Particulars List</Card.Header>
                 <Card.Body>
@@ -180,13 +184,13 @@ export default function Particulars() {
                               <Col>
                                 <ExportCSVButton
                                   {...props.csvProps}
-                                  className="btn-outline-secondary csv-button d-block d-md-none"
+                                  className="btn-outline-secondary d-block d-sm-none csv-button"
                                 >
                                   Export CSV
                                 </ExportCSVButton>
                               </Col>
                             </Row>
-                            <Row>
+                            <Row className="mt-1">
                               <Col>
                                 <SearchBar
                                   {...props.searchProps}
@@ -200,7 +204,7 @@ export default function Particulars() {
                                 />
                                 <ExportCSVButton
                                   {...props.csvProps}
-                                  className="btn-outline-secondary csv-button d-none d-md-block"
+                                  className="btn-outline-secondary d-none d-sm-block csv-button"
                                 >
                                   Export CSV
                                 </ExportCSVButton>
@@ -214,6 +218,7 @@ export default function Particulars() {
                                   striped
                                   hover
                                   condensed
+                                  wrapperClasses="table-responsive"
                                   {...props.baseProps}
                                 ></BootstrapTable>
                               </Col>
@@ -234,5 +239,5 @@ export default function Particulars() {
         </Layout>
       )}
     </>
-    )
+  );
 }

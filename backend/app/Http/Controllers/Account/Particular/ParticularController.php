58103ffa13929e7edda40ba9b448821acccbd78d
shoaib <shoaibhaque7@gmail.com>
2022-11-17 18:48:12 +0600

    added particular list api on statement create page

---
+++ backend/app/Http/Controllers/Account/Particular/ParticularController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Account\Particular;
+
+use App\Http\Controllers\Controller;
+use App\Models\Particulars;
+
+class ParticularController extends Controller
+{
+    /**
+     * Create a new ParticularController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:account');
+        config(['auth.defaults.guard' => 'account']);
+    }
+
+    /**
+     * Get Particulars List
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        return Particulars::select('*')->get();
+    }
+}

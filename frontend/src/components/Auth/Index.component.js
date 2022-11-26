1504c3d7904bb543d192cf2bd69fa7a8f1af7ae6
shoaib haque <shoaibhaque7@gmail.com>
2022-11-26 12:54:39 +0600

    resolved some warnings

--- frontend/src/components/Auth/Index.component.js
+++ frontend/src/components/Auth/Index.component.js
@@ -5,23 +5,20 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Badge from "react-bootstrap/Badge";
 import axios from "axios";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
-import Css from "./Index.css";
+import "./Index.css";
 
 export default function Login() {
   const navigate = useNavigate();
   const [bank_id, setBankId] = useState("");
   const [password, setPassword] = useState("");
   const [authError, setAuthError] = useState("");
-  const [validationError, setValidationError] = useState({});
 
   const login = async (e) => {
     e.preventDefault();
     setAuthError("");
-    setValidationError({});
     const formData = new FormData();
     formData.append("bank_id", bank_id);
     formData.append("password", password);
@@ -41,7 +38,6 @@ export default function Login() {
 
   const adminLogin = async () => {
     setAuthError("");
-    setValidationError({});
     const formData = new FormData();
     formData.append("username", bank_id);
     formData.append("password", password);
@@ -61,7 +57,6 @@ export default function Login() {
 
   const userLogin = async () => {
     setAuthError("");
-    setValidationError({});
     const formData = new FormData();
     formData.append("email", bank_id);
     formData.append("password", password);
@@ -77,9 +72,7 @@ export default function Login() {
       })
       .catch(({ response }) => {
         console.log(response);
-        if (response.status === 422) {
-          setValidationError(response.data);
-        } else if (response.status === 401) {
+        if (response.status === 401) {
           setAuthError(response.data.error);
         } else {
           Swal.fire({
